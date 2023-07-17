f930ecbf73f1ebb2da7ee6767d546ab9b94ed9c3
Sebastian Pola <seba.pola@icloud.com>
2023-07-17 18:49:17 +0200

    init

--- src/main/java/com/example/springsecurityexample/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityexample/config/SecurityConfig.java
@@ -1,2 +1,28 @@
-package com.example.springsecurityexample.config;public class SecurityConfig {
+package com.example.springsecurityexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("jola")
+                .password("{noop}aaa")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("bajtek")
+                .password("{noop}bbb")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
--- src/main/java/com/example/springsecurityexample/web/WebController.java
+++ src/main/java/com/example/springsecurityexample/web/WebController.java
@@ -1,2 +1,18 @@
-package com.example.springsecurityexample.web;public class WebController {
+package com.example.springsecurityexample.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        System.out.println(currentPrincipalName);
+        return "index";
+    }
 }
